6bc734c1b4a7b3502809682760b865433ce5dd24
Ganesh Bastapure <bastapureganesh21@gmail.com>
2024-07-30 22:30:13 +0530

    Update Content.js

--- components/Content.js
+++ components/Content.js
@@ -6,7 +6,7 @@ import Footer from "./Footer";
 
 const Content = () => {
   return (
-    <div className="bg-dark-900 w-full min-h-screen flex flex-col overflow-y-scroll">
+    <div className="bg-dark-900 w-full min-h-screen max-md:mt-24 flex flex-col overflow-y-scroll">
       <div id="about"/>
       <About />
       <div id="skills"/>
